e2db5492670c6655d7da679ab9421e1c6b96413f
Alex Orga <alexorga.dev@gmail.com>
2018-07-17 20:43:21 -0500

    added login screen with functioning input fields

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,13 @@ import firebase from 'firebase';
 import './App.css';
 
 import HomePage from '../components/HomePage/HomePage';
+import Navbar from '../components/Navbar/Navbar';
+import Login from '../components/Login/Login';
+import Register from '../components/Register/Register';
+import Dashboard from '../components/Dashboard/Dashboard';
+import AllTrips from '../components/AllTrips/AllTrips';
+import NewTrip from '../components/NewTrip/NewTrip';
+import AllSavedTrips from '../components/AllSavedTrips/AllSavedTrips';
 import fbConnection from '../firebaseRequests/connection';
 fbConnection();
 
@@ -42,12 +49,76 @@ const PublicRoute = ({component: Component, authed, ...rest}) => {
 };
 
 class App extends Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount () {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({authed: true});
+      } else {
+        this.setState({authed: false});
+      }
+    });
+  }
+
+  componentWillUnmount () {
+    this.removeListener();
+  }
+
+  runAway () {
+    this.setState({authed: false});
+  }
+
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Take Me There</h1>
-        </header>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Navbar
+              authed={this.state.authed}
+              runAway={this.runAway}
+            />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={HomePage}/>
+                </Switch>
+                <PublicRoute
+                  path="/register"
+                  authed={this.state.authed}
+                  component={Register}
+                />
+                <PublicRoute
+                  path="/login"
+                  authed={this.state.authed}
+                  component={Login}
+                />
+                <PrivateRoute
+                  path="/dashboard"
+                  authed={this.state.authed}
+                  component={Dashboard}
+                />
+                <PrivateRoute
+                  path="/alltrips"
+                  authed={this.state.authed}
+                  component={AllTrips}
+                />
+                <PrivateRoute
+                  path="/newtrip"
+                  authed={this.state.authed}
+                  component={NewTrip}
+                />
+                <PrivateRoute
+                  path="/savedtrips"
+                  authed={this.state.authed}
+                  component={AllSavedTrips}
+                />
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,63 @@
 import React from 'react';
+import authRequests from '../../firebaseRequests/auth';
+import { Link } from 'react-router-dom';
 
 import './Navbar.css';
 
 class Navbar extends React.Component {
   render () {
+    const {authed, runAway} = this.props;
+
+    const logoutClickEvent = () => {
+      authRequests.logoutUser();
+      runAway();
+    };
+
     return (
-      <h1 className="header">Navbar</h1>
+      <div className="NavBar text-center">
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <Link to="/dashboard" className="navbar-brand">TMT</Link>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              {
+                authed ? (
+                  <ul className="nav navbar-nav navbar-right">
+                    <li><Link to="/alltrips">
+                      All Trips</Link>
+                    </li>
+                    <li><Link to="/savedtrips">
+                      Saved Trips</Link>
+                    </li>
+                    <li className="navbar-form">
+                      <button
+                        onClick={logoutClickEvent}
+                        className="btn btn-danger"
+                      >
+                        Logout
+                      </button>
+                    </li>
+                  </ul>
+                ) : (
+                  <ul className="nav navbar-nav navbar-right">
+                    <li><Link to="/login">
+                      Login</Link>
+                    </li>
+                  </ul>
+                )
+              }
+
+            </div>
+          </div>
+        </nav>
+      </div>
     );
   }
 }
