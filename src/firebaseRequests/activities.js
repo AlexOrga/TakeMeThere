9ff2636dbb1de45038a244935d2fe2750e0cb04b
Alex Orga <alexorga.dev@gmail.com>
2018-07-28 18:40:28 -0500

    activities and countries arrays are returning in alphabetical order from the firebase get request

--- src/firebaseRequests/activities.js
+++ src/firebaseRequests/activities.js
@@ -15,6 +15,11 @@ const getActivities = () => {
             activitiesArray.push(newActivityObject);
           });
         }
+        activitiesArray.sort((a, b) => {
+          const textA = a.name.toLowerCase();
+          const textB = b.name.toLowerCase();
+          return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+        });
         resolve(activitiesArray);
       })
       .catch((err) => {
