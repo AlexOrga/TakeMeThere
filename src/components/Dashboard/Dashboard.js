2ea53b83056a1121c612de55044ae7e07a0e4c56
Alex Orga <alexorga.dev@gmail.com>
2018-08-11 15:40:23 -0500

    added background image to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,15 +6,18 @@ import './Dashboard.css';
 class Dashboard extends React.Component {
   render () {
     return (
-      <div className='container'>
-        <div>
-          <h2><Link to='./alltrips'>All Trips</Link></h2>
-        </div>
-        <div>
-          <h2><Link to='./savedtrips'>My Saved Trips</Link></h2>
-        </div>
-        <div>
-          <h2><Link to='./newtrip'>Create A New Trip</Link></h2>
+      <div>
+        <img className='dashboard-img' src="https://images.unsplash.com/photo-1510914828947-36f754990aa5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7a0fac6755d2fc8fcb4c3dd5f4255d6d&auto=format&fit=crop&w=3289&q=80" alt="Person holding a compass."/>
+        <div className='container'>
+          <div>
+            <h2><Link to='./alltrips'>All Trips</Link></h2>
+          </div>
+          <div>
+            <h2><Link to='./savedtrips'>My Saved Trips</Link></h2>
+          </div>
+          <div>
+            <h2><Link to='./newtrip'>Create A New Trip</Link></h2>
+          </div>
         </div>
       </div>
     );
