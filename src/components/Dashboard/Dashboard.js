5e7653c4c3a0275984c3e76784fce32655d30558
Alex Orga <alexorga.dev@gmail.com>
2018-08-11 16:10:15 -0500

    added border and border radius to dashboard elements still working on positioning

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -9,14 +9,20 @@ class Dashboard extends React.Component {
       <div>
         <img className='dashboard-img' src="https://images.unsplash.com/photo-1510914828947-36f754990aa5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7a0fac6755d2fc8fcb4c3dd5f4255d6d&auto=format&fit=crop&w=3289&q=80" alt="Person holding a compass."/>
         <div className='container'>
-          <div>
-            <h2><Link to='./alltrips'>All Trips</Link></h2>
+          <div className='col-sm-6 text-center'>
+            <div className='dashboard-element'>
+              <h2><Link to='./alltrips'>All Trips</Link></h2>
+            </div>
           </div>
-          <div>
-            <h2><Link to='./savedtrips'>My Saved Trips</Link></h2>
+          <div className='col-sm-6 text-center'>
+            <div className='dashboard-element'>
+              <h2><Link to='./savedtrips'>My Saved Trips</Link></h2>
+            </div>
           </div>
-          <div>
-            <h2><Link to='./newtrip'>Create A New Trip</Link></h2>
+          <div className='col-sm-12 text-center'>
+            <div className='dashboard-element'>
+              <h2><Link to='./newtrip'>Create A New Trip</Link></h2>
+            </div>
           </div>
         </div>
       </div>
