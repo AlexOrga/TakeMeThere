5568a3a08a71b0cac4f29a26876830cb2da8c2f1
Alex Orga <alexorga.dev@gmail.com>
2018-08-13 13:09:38 -0500

    improved colors for links on dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,21 +8,27 @@ class Dashboard extends React.Component {
     return (
       <div>
         <img className='dashboard-img' src="https://images.unsplash.com/photo-1510914828947-36f754990aa5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7a0fac6755d2fc8fcb4c3dd5f4255d6d&auto=format&fit=crop&w=3289&q=80" alt="Person holding a compass."/>
-        <div className='container'>
-          <div className='col-sm-6 text-center'>
-            <div className='dashboard-element'>
-              <h2><Link to='./alltrips'>All Trips</Link></h2>
-            </div>
+        <div className='container main-element-container'>
+          <div className='col-sm-4 text-center single-element-container'>
+            <Link to='./alltrips'>
+              <div className='dashboard-element'>
+                <h2>All Trips</h2>
+              </div>
+            </Link>
           </div>
-          <div className='col-sm-6 text-center'>
-            <div className='dashboard-element'>
-              <h2><Link to='./savedtrips'>My Saved Trips</Link></h2>
-            </div>
+          <div className='col-sm-4 text-center single-element-container'>
+            <Link to='./savedtrips'>
+              <div className='dashboard-element'>
+                <h2>My Saved Trips</h2>
+              </div>
+            </Link>
           </div>
-          <div className='col-sm-12 text-center'>
-            <div className='dashboard-element'>
-              <h2><Link to='./newtrip'>Create A New Trip</Link></h2>
-            </div>
+          <div className='col-sm-4 text-center single-element-container'>
+            <Link to='./newtrip'>
+              <div className='dashboard-element'>
+                <h2>Create A New Trip</h2>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
